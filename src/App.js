02f5ee7dc34a949403753753d2727f020f64b90c
replexor <replexor@ya.ru>
2020-12-30 21:50:31 +0300

    Hello world

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Место для чата
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
