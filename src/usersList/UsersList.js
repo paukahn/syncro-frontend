567688eb55334d5583fd3d042710a431e45593a8
replexor <replexor@ya.ru>
2021-07-20 00:36:09 +0300

    Change structure: Added user list files

---
+++ src/usersList/UsersList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './ULStyles.css';
+
+export const UsersList = () => {
+    return (
+        <div class="info_list_users">
+            <div class="list_element">
+                <div class="list_element_wrap">
+                    <div class="element_image"></div>
+                    <div class="right_wrap">
+                        <div class="up_wrap">
+                            <div class="element_user">Мухолов Ван Хельсинг</div>
+                            <div class="element_time">20:04</div>
+                        </div>
+                        <p class="element_message">From: Нормальный вариант</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="list_element">
+                <div class="list_element_wrap">
+                    <div class="element_image"></div>
+                    <div class="right_wrap">
+                        <div class="up_wrap">
+                            <div class="element_user">Борис Леонидович Качерговский</div>
+                            <div class="element_time">00:01</div>
+                        </div>
+                        <p class="element_message">To: Длинное имя</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="list_element">
+                <div class="list_element_wrap">
+                    <div class="element_image"></div>
+                    <div class="right_wrap">
+                        <div class="up_wrap">
+                            <div class="element_user">Денис Денисов</div>
+                            <div class="element_time">15:30</div>
+                        </div>
+                        <p class="element_message">To: Очень длинный текст, который нифига не помещается</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
