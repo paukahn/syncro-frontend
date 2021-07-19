947cde8ed609da6dc6c2fe1bb27bd9585de3f170
replexor <replexor@ya.ru>
2021-07-20 00:35:48 +0300

    Change structure: Added search section files

---
+++ src/search/SearchSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SSStyles.css';
+
+export const SearchSection = () => {
+    return (
+        <div class="info_list_search">
+            <input type="text" class="search" placeholder="Поиск" />
+        </div>
+    );
+};
